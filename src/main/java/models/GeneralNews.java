e4f864be6a71577c459483c3a720567ab2ffa3cb
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 17:49:47 +0300

    constructor generated

--- src/main/java/models/GeneralNews.java
+++ src/main/java/models/GeneralNews.java
@@ -1,4 +1,11 @@
 package models;
 
-public class GeneralNews {
+public class GeneralNews extends News{
+    private static final String TYPE="General News";
+
+    public GeneralNews(String content) {
+        this.content=content;
+        this.type=TYPE;
+    }
+
 }
