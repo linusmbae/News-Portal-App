32ddacf74e4c5757775ed5f60a07689cb80bb9a9
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 16:50:16 +0300

    getters returns the correct informarion

--- src/test/java/models/UsersTest.java
+++ src/test/java/models/UsersTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,35 @@ public class UsersTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getName_returnsCorrectName()throws Exception {
+        Users testUser=createUser();
+        assertEquals("Linus",testUser.getName());
+    }
+
+    @Test
+    public void getEmail_returnsCorrectEmail()throws Exception {
+        Users testUser=createUser();
+        assertEquals("linus@gmail.com",testUser.getEmail());
+    }
+
+    @Test
+    public void getPhone_returnsCorrectPhone()throws Exception {
+        Users testUser=createUser();
+        assertEquals("0714719935",testUser.getPhone());
+    }
+
+    @Test
+    public void getIdNumber_returnsCorrectIdNumber()throws Exception {
+        Users testUser=createUser();
+        assertEquals(12345,testUser.getIdNumber());
+    }
+
+    //    HELPERS
+    public Users createUser()
+    {
+        return new Users("Linus","linus@gmail.com","0714719935",12345);
     }
 }
