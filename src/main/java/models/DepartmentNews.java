83f25e2e569162ebeb824928a80886f657805216
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 18:03:28 +0300

    generate object constructor

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,5 +1,10 @@
 package models;
 
-public class DepartmentNews {
+public class DepartmentNews extends News {
+    private static final String TYPE="Department News";
 
+    public DepartmentNews(String content) {
+        this.content=content;
+        this.type=TYPE;
+    }
 }
