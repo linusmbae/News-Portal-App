4e3ee6d442a91086e4c84a7faa5bb831d01a85cb
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 17:12:01 +0300

    generate constructors

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,14 @@
 package models;
 
 public class Department {
+private int id;
+private String name;
+private String floor;
+
+    public Department(String name, String floor) {
+        this.name = name;
+        this.floor = floor;
+    }
+
 
 }
