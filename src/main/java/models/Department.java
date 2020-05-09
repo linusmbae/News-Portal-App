b6deef140ddf1397f8ebe77fc08e5fb256f38f4f
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 17:12:35 +0300

    generate setters

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -10,5 +10,27 @@ private String floor;
         this.floor = floor;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
 }
