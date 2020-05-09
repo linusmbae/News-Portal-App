73e90d870f0ed94812f87d9bb45f97635018e501
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 17:33:26 +0300

    create news abstract class and generate getters and setters

---
+++ src/main/java/models/News.java
@@ -0,0 +1,31 @@
+package models;
+
+public abstract class News {
+    public int id;
+    public String content;
+    public String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
