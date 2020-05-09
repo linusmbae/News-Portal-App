8821d6ab1049eaf4ae7c2200c78a70ae6fbd1b04
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 16:13:22 +0300

    create private members and constructor

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Users {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String idNumber;
 
+    public Users(String name, String email, String phone, String idNumber) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.idNumber = idNumber;
+    }
 }
