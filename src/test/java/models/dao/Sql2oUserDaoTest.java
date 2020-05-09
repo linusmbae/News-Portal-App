194d2e04dc7678ba88f843326b9b22e6bc011781
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-09 22:01:33 +0300

    create user dao test

---
+++ src/test/java/models/dao/Sql2oUserDaoTest.java
@@ -0,0 +1,20 @@
+package models.dao;
+
+import org.junit.After;
+import org.junit.Before;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
+
+public class Sql2oUserDaoTest {
+    private static Connection conn;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
