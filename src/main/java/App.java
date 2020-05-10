4bf664e0ced6d35660667d6e75601557de4cb417
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-10 21:37:02 +0300

    department json complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import models.Department;
+import models.GeneralNews;
 import models.Users;
 import models.dao.Sql2oDepartmentDao;
 import models.dao.Sql2oNewsDao;
@@ -61,12 +62,101 @@ public class App {
         {
             int userId=Integer.parseInt(request.params("id"));
             Users users=userDao.findById(userId);
-            userDao.removeById(users.getId());
-            response.status(201);
-            return gson.toJson(users);
+            if (users!=null)
+            {
+                userDao.removeById(users.getId());
+                return gson.toJson(departmentDao.getAll());
+            }else
+            {
+                return gson.toJson(departmentDao.getAll());
+            }
+
         });
 
+        post("/users/clear","application/json",(request, response) ->
+        {
+            System.out.println(userDao.getAll());
+            if (userDao.getAll().size()>0)
+            {
+                userDao.clearAll();
+                return gson.toJson(userDao.getAll());
+            }else
+            {
+                return gson.toJson(userDao.getAll());
+            }
+        });
+
+//        DEPARTMENT
+        post("/department/new","application/json",(request, response) ->
+        {
+            Department department=gson.fromJson(request.body(),Department.class);
+            departmentDao.save(department);
+            response.status(201);
+            return gson.toJson(department);
+        });
+
+        get("/departments","application/json",(request, response) ->
+        {
+            System.out.println(departmentDao.getAll());
+            if (departmentDao.getAll().size()>0) {
+                return gson.toJson(departmentDao.getAll());
+            }else
+            {
+                return "{\\\"message\\\":\\\"I'm sorry, but no departments are currently listed in the database.\\\"}";
+            }
+        });
+
+        get("/department/:id","application/json",(request, response) ->
+        {
+            int departmentId=Integer.parseInt(request.params("id"));
+            Department findDepartment=departmentDao.findById(departmentId);
+            if (findDepartment!=null)
+            {
+                return gson.toJson(findDepartment);
+            }else
+            {
+                return "{\\\"message\\\":\\\"I'm sorry, but no departments are currently listed in the database.\\\"}";
+            }
+        });
+
+//        post("department/:id/update","application.json",(request, response) ->
+//        {
+//            int departmentId=Integer.parseInt(request.params("id"));
+//            Department findDepartment=departmentDao.findById(departmentId);
 //
+//
+//            if (findDepartment!=null)
+//            {
+//                departmentDao.update();
+//
+//            }
+//        });
+
+        post("/departments/:id/remove","application/json",(request, response) ->
+        {
+            System.out.println(departmentDao.getAll());
+            int departmentId=Integer.parseInt(request.params("id"));
+            Department departmentToRemove=departmentDao.findById(departmentId);
+            if (departmentToRemove!=null) {
+                departmentDao.deleteById(departmentToRemove.getId());
+                return gson.toJson(departmentDao.getAll());
+            }else
+            {
+                return gson.toJson(departmentDao.getAll());
+            }
+        });
+        post("/departments/remove/all","application/json",(request, response) ->
+        {
+            System.out.println(departmentDao.getAll());
+            if (departmentDao.getAll().size()>0) {
+                departmentDao.clearAll();
+                return gson.toJson(departmentDao.getAll());
+            }else
+            {
+                return "{\\\"message\\\":\\\"I'm sorry, but no departments are currently listed in the database.\\\"}";
+            }
+        });
+
 //        FILTERS
         after((req, res) ->{
             res.type("application/json");
