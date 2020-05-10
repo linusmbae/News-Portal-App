006081a7d6b2df4ac1443f440d0b71c0b3d8545f
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-10 18:42:04 +0300

    user json complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import com.google.gson.Gson;
+import models.Department;
+import models.Users;
 import models.dao.Sql2oDepartmentDao;
 import models.dao.Sql2oNewsDao;
 import models.dao.Sql2oUserDao;
@@ -21,6 +23,53 @@ public class App {
         userDao=new Sql2oUserDao(sql2o);
         conn=sql2o.open();
 
+//        USERS
+        post("/users/new","application/json",(request, response) ->
+        {
+            Users users=gson.fromJson(request.body(),Users.class);
+            userDao.save(users);
+            response.status(201);
+            return gson.toJson(users);
+        });
 
+        get("/users","application/json",(request, response) ->
+        {
+            System.out.println(userDao.getAll());
+            if (userDao.getAll().size()>0)
+            {
+                return gson.toJson(userDao.getAll());
+            }else
+            {
+                return "{\\\"message\\\":\\\"I'm sorry, but no users are currently listed in the database.\\\"}";
+            }
+        });
+
+        get("/users/:id","application/json",(request, response) ->
+        {
+           int userId=Integer.parseInt(request.params("id"));
+           Users findUser=userDao.findById(userId);
+           if (findUser!=null)
+           {
+               return gson.toJson(findUser);
+           }else
+           {
+               return "{\\\"message\\\":\\\"I'm sorry, but no users are currently listed in the database.\\\"}";
+           }
+        });
+
+        post("/users/:id/remove","application/json",(request, response) ->
+        {
+            int userId=Integer.parseInt(request.params("id"));
+            Users users=userDao.findById(userId);
+            userDao.removeById(users.getId());
+            response.status(201);
+            return gson.toJson(users);
+        });
+
+//
+//        FILTERS
+        after((req, res) ->{
+            res.type("application/json");
+        });
     }
 }
