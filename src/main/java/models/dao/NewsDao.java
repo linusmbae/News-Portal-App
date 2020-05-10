d466993b6573e997aee941fe15a936976fe344ca
Linus Mutugi <linusmutugi5178@gmail.com>
2020-05-10 13:36:04 +0300

    news dao interface complete

--- src/main/java/models/dao/NewsDao.java
+++ src/main/java/models/dao/NewsDao.java
@@ -1,5 +1,27 @@
 package models.dao;
 
+import models.DepartmentNews;
+import models.GeneralNews;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+//    CREATE
+    void saveDepartmentNews(DepartmentNews departmentNews);
+    void saveGeneralNews(GeneralNews generalNews);
+
+
+//    READ
+    List<News>getAll();
+    List<DepartmentNews> getDepartmentNews(int departmentNewsId);
+    List<GeneralNews>getGeneralNews(int generalNewsId);
+
+
+//    UPDATE
+
+//    DELETE
+    void clearAll();
+    void removeById(int id);
 
 }
